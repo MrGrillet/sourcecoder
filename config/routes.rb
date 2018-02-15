b42341fc11d55fd86dd04bedbef32c43c6649b4a
Peter <pagrillet@gmail.com>
2018-02-15 19:01:06 +0000

    Added my custom skin to Bootstrap and worked out how to add images to rails applications. Also added a menu to make navigation possible.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :jobs
-
+  get 'admin/elements', to: 'admin#elements'
+  get 'admin', to: 'admin#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
