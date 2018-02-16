dacc5552caf28210cd83ff573cf9d408c40b4fbd
Peter <pagrillet@gmail.com>
2018-02-16 17:06:21 +0000

    It's now possible for a user to select the type of user they want to sign up as when they first sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'home#index'
   resources :jobs
   get 'admin/elements', to: 'admin#elements'
   get 'admin', to: 'admin#index'
 
   resources :user_jobs, only: [:create]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
