78c8aabb11b1fcd20e90dd8fb32165661806063a
Peter <pagrillet@gmail.com>
2018-02-17 14:08:47 +0000

    The ability to edit and delete job posts has been limited to admins and those that created the post.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,27 @@
 module ApplicationHelper
 
 	def admin_user?
-		if current_user.user_type == "admin"
+		if current_user && current_user.user_type == "admin"
 			true
 		else
 			false
 		end 
 	end
+
+	def job_owner?
+		 # Not working. Can't work out why
+
+		if current_user && current_user.id == job.user_id
+			true
+		else
+			false
+		end 
+		
+	end
 	
 
 	def employer?
-		if current_user.user_type == "employer"
+		if current_user && current_user.user_type == "employer"
 			true
 		else
 			false
@@ -18,16 +29,15 @@ module ApplicationHelper
 	end
 
 	def applicant?
-		if current_user.user_type == "applicant"
+		if current_user && current_user.user_type == "applicant"
 			true
 		else
 			false
 		end 
 	end
 
-def user_id
-	current_user.id
-	
-end
+	def user_id
+	 current_user.id
+	end
 
 end
