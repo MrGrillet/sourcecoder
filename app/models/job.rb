f19c0b9035429ca335169c97605960a9eae3b681
Peter <pagrillet@gmail.com>
2018-02-26 14:30:18 +0000

    It's now possible to search multiple job fields. I haven't worked out how I am going to be able to search related tables, but I have a few ideas for some hacks. For now, this search is passable. I actually need to add some job posts that have different text in them. I have run a couple tests which worked but I want to see what happens when real job ads are added. Also, searches are no longer case sensitive.

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,11 +5,15 @@ class Job < ActiveRecord::Base
 	has_many :code_languages, through: :job_languages
 	#validates :user_id, presance: true
 
-def self.search(term)
-  if term
-    where('title LIKE ?', "%#{term}%")
-  else
-    all
-  end
+	def self.search(term)
+		if term
+			where("title ilike :q or description ilike :q or the_role ilike :q or location ilike :q or applicant_background ilike :q or skills_essential ilike :q or skills_ideal ilike :q or tech_stack ilike :q or benefits ilike :q", q: "%#{term}%")
+		else
+			all
+		end
+	end
 end
-end
+
+
+
+    
